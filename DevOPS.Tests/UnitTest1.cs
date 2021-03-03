2092ca6b700489f419a9692980aea712fa8a81a8
Kevin Quiercelin <quiercelinkevin@gmail.com>
2021-03-03 14:32:22 +0100

    Commit Test

--- DevOPS.Tests/UnitTest1.cs
+++ DevOPS.Tests/UnitTest1.cs
@@ -17,8 +17,37 @@ namespace DevOPS.Tests
             Calculatrice calc = new Calculatrice();
             int result = calc.add(a, b);
 
+            Assert.AreEqual(6, result);
+        }
 
-            Assert.AreEqual(6,result);
+        public void TestSu()
+        {
+            int a = 2;
+            int b = 8;
+            Calculatrice calc = new Calculatrice();
+            int result = calc.sub(a, b);
+
+            Assert.AreEqual(6, result);
+
+        }
+        public void TestMul()
+        {
+            int a = 2;
+            int b = 7;
+            Calculatrice calc = new Calculatrice();
+            int result = calc.mul(a, b);
+
+            Assert.AreEqual(6, result);
+
+        }
+        public void TestDiv()
+        {
+            int a = 7;
+            int b = 4;
+            Calculatrice calc = new Calculatrice();
+            int result = calc.div(a, b);
+
+            Assert.AreEqual(6, result);
 
         }
     }
--- DevOPS/Calculatrice.cs
+++ DevOPS/Calculatrice.cs
@@ -18,6 +18,18 @@ namespace DevOPS
         {
             return a - b;
         }
-
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+        public int div(int a, int b)
+        {
+            int result = 0;
+            if (b != 0)
+            {
+                result = a / b;
+            }
+            return result;
+        }
     }
 }
